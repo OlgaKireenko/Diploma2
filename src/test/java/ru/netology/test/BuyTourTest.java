9dffee2e4bcf79ea03708f70a55c23e2de95ec5d
Kireenko Olga <kireenko86.osil@mail.ru>
2023-03-26 18:39:29 +0300

    add some tests

--- src/test/java/ru/netology/test/BuyTourTest.java
+++ src/test/java/ru/netology/test/BuyTourTest.java
@@ -1,6 +1,7 @@
 package ru.netology.test;
  ///import lombok.var;
 ///import lombok.var;
+
 import ru.netology.page_object.BuyTourPage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -24,16 +25,89 @@ class BuyTourTest {
     @Test
     @DisplayName("Should Successful Buy Tour By Card")
     void shouldSuccessfulBuyTourByCard() {
-     var buyTourPage = new DashboardPage().clickButtonBuy();
+        var buyTourPage = new DashboardPage().clickButtonBuy();
+        DashboardPage.PutActiveCardNumber();
+        DashboardPage.PutValidMonth();
+        DashboardPage.PutValidYear();
+        DashboardPage.PutValidCVV();
+        DashboardPage.PutValidOwner();
+        $("form button").click();
+        $(".notification__content").shouldBe(visible, Duration.ofMinutes(2)).shouldHave(exactText("Успешно. Операция одобрена банком"));
 
-        buyTourPage.PutActiveCardNumber();
-        buyTourPage.PutValidMonth();
-        buyTourPage.PutValidYear();
-        buyTourPage.PutValidCVV();
-        buyTourPage.PutValidOwner();
-        $(byText("Продолжить")).parent().$("button");/////???не могу найти кнопку
-        ///$(".notification__content").shouldBe(visible, Duration.ofSeconds(30)).shouldHave(exactText(""));
+    }
 
+    @Test
+    @DisplayName("Should reject Buy Tour By not valid Card")
+    void shouldRejectBuyTourByNotValidCard() {
+        var buyTourPage = new DashboardPage().clickButtonBuy();
+        DashboardPage.PutNotActiveCardNumber();
+        DashboardPage.PutValidMonth();
+        DashboardPage.PutValidYear();
+        DashboardPage.PutValidCVV();
+        DashboardPage.PutValidOwner();
+        $("form button").click();
+        $(".notification__content").shouldBe(visible, Duration.ofMinutes(2)).shouldHave(text("Ошибка! Банк отказал в проведении операции"));
+    }
+
+    @Test
+    @DisplayName("Should reject Buy Tour By With Short Card Number")
+    void shouldRejectBuyTourWithShortCardNumber() {
+        var buyTourPage = new DashboardPage().clickButtonBuy();
+        DashboardPage.PutShortCardNumber();
+        DashboardPage.PutValidMonth();
+        DashboardPage.PutValidYear();
+        DashboardPage.PutValidCVV();
+        DashboardPage.PutValidOwner();
+        $("form button").click();
+        $(byText("Неверный формат")).shouldBe(visible);
+    }
+    @Test
+    @DisplayName("Should reject Buy Tour By Without Card Number")
+    void shouldRejectBuyTourWithoutCardNumber() {
+        var buyTourPage = new DashboardPage().clickButtonBuy();
+        DashboardPage.PutValidMonth();
+        DashboardPage.PutValidYear();
+        DashboardPage.PutValidCVV();
+        DashboardPage.PutValidOwner();
+        $("form button").click();
+        $(byText("Неверный формат")).shouldBe(visible);
+    }
+
+    @Test
+    @DisplayName("Should reject Buy Tour with Letters in Card Number")
+    void shouldRejectBuyTourWithLettersInCardNumber() {
+        var buyTourPage = new DashboardPage().clickButtonBuy();
+        DashboardPage.PutLettersInCardNumber();
+        DashboardPage.PutValidMonth();
+        DashboardPage.PutValidYear();
+        DashboardPage.PutValidCVV();
+        DashboardPage.PutValidOwner();
+        $("form button").click();
+        $(byText("Неверный формат")).shouldBe(visible);
+    }
+
+    @Test
+    @DisplayName("Should reject Buy Tour with Not Valid Month")
+    void shouldRejectBuyTourWithNotValidMonth() {
+        var buyTourPage = new DashboardPage().clickButtonBuy();
+        DashboardPage.PutActiveCardNumber();
+        DashboardPage.PutNotValidMonth();
+        DashboardPage.PutValidYear();
+        DashboardPage.PutValidCVV();
+        DashboardPage.PutValidOwner();
+        $("form button").click();
+        $(byText("Неверно указан срок действия карты")).shouldBe(visible);
+    }
+
+    @Test
+    @DisplayName("Should reject Buy Tour with Not Valid Month(Tree symbols)")
+    void shouldRejectBuyTourWithTreeSymbolsInMonth() {
+        var buyTourPage = new DashboardPage().clickButtonBuy();
+        DashboardPage.PutActiveCardNumber();
+        DashboardPage.PutTreeSymbolsInMonth();
+        $("form button").click();
 
     }
 }
+
+

e50ed2fc98edca6451c99caa911eb9b98a0b6e77
Kireenko Olga <kireenko86.osil@mail.ru>
2023-05-08 16:08:56 +0300

    add gate-simul

---
+++ src/test/java/ru/netology/sql/SqlQuery.java
@@ -0,0 +1,54 @@
+package ru.netology.sql;
+
+import lombok.SneakyThrows;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class SqlQuery {
+    private static final String DB_URL = System.getProperty("datasource.url");
+
+    private SqlQuery() {
+
+    }
+
+    @SneakyThrows
+    public static Connection getConnection() {
+        return DriverManager.getConnection(DB_URL, "app", "pass");
+    }
+
+    @SneakyThrows
+    public static void clearDB() {
+        var runner = new QueryRunner();
+        var deleteSQL = "DELETE FROM credit_request_entity;";
+        try (var connection = getConnection()) {
+            runner.execute(connection, deleteSQL);
+
+            //to do параметризовать?
+            runner.execute(connection, "DELETE FROM order_entity");
+            runner.execute(connection, "DELETE FROM payment_entity");
+        }
+    }
+
+    @SneakyThrows
+    public static String getDebitPaymentStatus() {
+        var runner = new QueryRunner();
+        var SqlStatus = "SELECT status FROM payment_entity ORDER BY created DESC LIMIT 1";
+        try (var connection = getConnection()) {
+            String result = runner.query(connection, SqlStatus, new ScalarHandler<>());
+            return result;
+        }
+    }
+
+    @SneakyThrows
+    public static String getCreditPaymentStatus() {
+        var runner = new QueryRunner();
+        String SqlStatus = "SELECT status FROM credit_request_entity ORDER BY created DESC LIMIT 1";
+        try (var connection = getConnection()) {
+            String result = runner.query(connection, SqlStatus, new ScalarHandler<>());
+            return result;
+        }
+    }
+}
